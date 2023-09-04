79331e1d2671e850c484fcb0e913b4894ffa0c02
Pietro907 <pietrop.puri@gmail.com>
2023-09-04 19:55:21 +0200

    add fix snack bonus

--- jsnackbonus/js/script.js
+++ jsnackbonus/js/script.js
@@ -11,34 +11,14 @@ const list = [];
 
 //Chiedo sei volte all'utente di inserire un numero
 for(let i = 0; i < 6; i++) {
-    numberUser.push( Number(prompt('inserisci un numero')));
+    list.push( Number(prompt('inserisci un numero')));
     console.log(list);
+
     //Se è dispari lo aggiungo alla lista vuota
-    if (numberUser % 3 !== 0) {
-       list.push(numberUser);
+    if (list[i] % 2 !== 0) {
+       numberUser.push(list[i]);
     }
 }
+//Stampo il la lista nella console
 console.log(numberUser);
 
-
-/* // array vuoto
-const numbers = [];
-const userNumbers = [];
-
-// chiedo all'utente di inserire 6 numeri casuali
-for (let i = 0; i < 6; i++) {
-    
-    userNumbers.push(Number(prompt(`Tasks ${i}, type here a random number.`)));
-    // loggo in console i numeri inseriti dall'utente
-    console.log(userNumbers);
-
-    // se il numero inserito è dispari lo inserisco nell'array e vado al prossimo numero fino a controllarli tutti
-    // se il numero inserito è pari lo ignoro e passo al prossimo numero fino a controllarli tutti
-    if (userNumbers[i] % 2 !== 0) {
-
-        numbers.push(userNumbers[i]);
-
-    }
-    
-}
-console.log(numbers); */
