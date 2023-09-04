01bdee614ade8bbe396958c291d30fe867715f02
Pietro907 <pietrop.puri@gmail.com>
2023-09-04 17:52:08 +0200

    add miss result to video

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -9,12 +9,12 @@ Il programma stampa la somma di tutti i numeri inseriti.
 const numberList = [Number(prompt('inserisci dieci numeri 1')) + Number(prompt('inserisci dieci numeri 2')) + Number(prompt('inserisci dieci numeri 3')) + Number(prompt('inserisci dieci numeri 4')) + Number(prompt('inserisci dieci numeri 5')) + Number(prompt('inserisci dieci numeri 6') + Number(prompt('inserisci dieci numeri 7')) + Number(prompt('inserisci dieci numeri 8')) + Number(prompt('inserisci dieci numeri 9')) + Number(prompt('inserisci dieci numeri 10')))];
 console.log(numberList);
 // poi fai un ciclo per sommare i valori dell’array
-for (i = 0; i <= 10; i++){
-    const numberUser = numberList[i];
-    console.log('totale = ' + numberUser)
-    const summaryElement = document.querySelector('.summary_ten_number');
-    summaryElement.innerHTML = numberUser;
+const numberUser = 0;
+for (i = 0; i <= numberList.length; i++){
+    let totale =+ numberList[i];
+    let inputTotale = document.querySelector('.summary_ten_number')
+    inputTotale.innerHTML = ('risultato: ' + totale);
 }
 //poi fai la stampa della somma
 //Associo elemento della DOM ad una constante
-console.log(summary);
+
